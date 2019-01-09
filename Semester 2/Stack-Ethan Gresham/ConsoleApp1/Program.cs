d021e7b17d9331c20c1c859aa95d28bb8990c21f
Franejo <franejotwitch@gmail.com>
2019-01-09 14:35:35 -0700

    Misadventures of FlapStack ~ to quote hunter

---
+++ Semester 2/Stack-Ethan Gresham/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            int whytho;
+            do
+            {
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                Console.WriteLine("1-Push^^^^^^^^^^^^^^^^+");
+                Console.WriteLine("2-Pop^^^^^^^^^^^^^^^^^+");
+                Console.WriteLine("3-Peek^^^^^^^^^^^^^^^^+");
+                Console.WriteLine("4-Print^^^^^^^^^^^^^^^+");
+                Console.WriteLine("5-Exit Program^^^^^^^^+");
+                Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                whytho = int.Parse(Console.ReadLine());
+
+                if (whytho == 1)
+                {
+                    Console.WriteLine("Yo chungus put in a numba");
+                    int numb = int.Parse(Console.ReadLine());
+
+                    myStack.Push(numb);
+                }
+                if (whytho == 2)
+                {
+                    
+                   Console.WriteLine(myStack.Pop());
+                }
+                if (whytho == 3)
+                {
+                    Console.WriteLine(myStack.Peek());
+                }
+                if (whytho == 4)
+                {
+                    myStack.Print();
+                }
+            } while (whytho != 5);
+           
+        }
+
+    }
+}
