2c6aef08bf558807d3ce28d6ab8e6691769c68f5
Franejo <franejotwitch@gmail.com>
2019-01-17 14:20:36 -0700

    Robloc

---
+++ Semester 2/Deck/Deck/card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck
+{
+    class card
+    {
+        public string suit = "";
+        public string face = "";
+        public card(string suit,string face)
+        {
+            this.suit = suit;
+            this.face = face;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~\n" + face + "of" + suit + "\n~~~~~~~~~~~~~~~~~~");
+        }
+    }
+}
